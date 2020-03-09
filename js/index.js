a7e408529ad1bc2b48af572158e7839f15caafb4
Tereshkov Sergey[L] <rat87@mail.ru>
2020-03-09 19:32:27 +0300

    bug: rewrite data

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ function initApp() {
     let sendMessageButton = document.querySelector('.main-conversationPanel-message_button');
     var sendMessageInput = document.querySelector('.main-conversationPanel-message_input');
     let chat = document.querySelector('.main-conversationPanel-chat');
+    let userPanel = document.querySelector('.main-userPanel');
     let logInBtn = document.querySelector('.popup-form__btn');
     let authWindow = document.querySelector('.overlay');
     let socket = new WebSocket('ws://localhost:8080');
@@ -13,40 +14,76 @@ function initApp() {
 
         let client = {
             name: logInName,
-            nick: logInNick
+            nick: logInNick,
+            action: 'logInAct'
         };
+        socket.send(JSON.stringify(client));
+        console.log('отправили на сервер залогиненного клиента', client);
         return client;
     }
 
     logInBtn.addEventListener('click', e => {
         console.log('Нажата');
-        clientData = logIn();
-        console.log(clientData);
+        logIn();
+        // console.log(clientData);
+        // sendMessage();
         authWindow.style.display = 'none';
 
     });
 
     function addMessage(message) {
-        console.log('Сообщение:', message);
-        let messageItem = document.createElement('div');
-        messageItem.className = 'message-item';
-        messageItem.textContent = message;
+        message = JSON.parse(message);
+        if (message.action == 'messageAct') {
+            console.log('Сообщение:', message);
 
-        chat.appendChild(messageItem);
-        chat.scrollTop = chat.scrollHeight;
+
+            let messageItem = document.createElement('div');
+            messageItem.className = 'message-item';
+            messageItem.textContent = `${message.name} (${message.nick}): ${message.message}, ${message.avatar}, ${message.action}`;
+
+            chat.appendChild(messageItem);
+            chat.scrollTop = chat.scrollHeight;
+        }
+
+        if (message.action == 'logInAct') {
+            console.log('Юзер:', message);
+            clientData = message;
+
+            let userItem = document.createElement('div');
+            let iconItem = document.createElement('div');
+            let nameItem = document.createElement('div');
+
+
+            userItem.className = 'user-item';
+            iconItem.className = 'icon-item';
+            nameItem.className = 'name-item';
+
+            nameItem.textContent = message.name;
+
+            userItem.appendChild(iconItem);
+            userItem.appendChild(nameItem);
+            userPanel.appendChild(userItem);
+        }
+
     }
 
-    socket.addEventListener('message', e => {
+
+    socket.onmessage = function (e) {
         console.log(e.data);
         addMessage(e.data);
-    });
+    };
 
     function sendMessage() {
+
         clientData.message = sendMessageInput.value;
+        clientData.action = 'messageAct';
+
 
         socket.send(JSON.stringify(clientData));
         console.log(JSON.stringify(clientData));
         sendMessageInput.value = '';
+
+
     }
 
     sendMessageButton.addEventListener('click', sendMessage);
--- js/server.js
+++ js/server.js
@@ -4,15 +4,57 @@ const wss = new WebSocket.Server({
     port: 8080
 });
 
+let user;
+
 wss.on('connection', function connection(ws) {
+
     ws.on('message', function incoming(data) {
+        user = {
+            name: 'пустое имя',
+            nick: 'пустой ник',
+            message: 'пустое сообщение',
+            avatar: 'пустой аватар',
+            action: 'пустое действие'
+        };
+
+        data = JSON.parse(data);
+        // console.log(data);
+        // console.log(data.name);
+        // console.log(data.nick);
+        // console.log(data.message);
+        // console.log(data.avatar);
+        // console.log(data.action);
+
+        user = {
+            name: data.name,
+            nick: data.nick,
+            message: data.message,
+            avatar: data.avatar,
+            action: data.action
+        };
+        console.log('===', user, '===');
+
+
         wss.clients.forEach(function each(client) {
+
             if (client.readyState === WebSocket.OPEN) {
-                data = JSON.parse(data);
-                console.log(data);
-                console.log(data.message);
-                client.send(`${data.name} (${data.nick}): ${data.message}`);
+                if (user.action == 'logInAct') {
+                    user = JSON.stringify(user);
+                    client.send(user);
+                    console.log('logInAct', data);
+                } else {
+                    user = JSON.stringify(user);
+                    //client.send(`${user.name} (${user.nick}): ${user.message}, ${user.avatar}, ${user.action}`);
+                    client.send(user);
+                    console.log('elseAct', user);
+                }
+
+
             }
         });
     });
 });
+
+function createUser(name) {
+
+}
