b53dc18f048a22d9668cdcd0e97531c14dc7c0a4
Tereshkov Sergey[L] <rat87@mail.ru>
2020-03-12 21:46:26 +0300

    codestyle fixed

--- js/index.js
+++ js/index.js
@@ -4,19 +4,13 @@ let currName;
 
 function initApp() {
     let sendMessageButton = document.querySelector('.main-conversationPanel-message_button');
-    var sendMessageInput = document.querySelector('.main-conversationPanel-message_input');
+    let sendMessageInput = document.querySelector('.main-conversationPanel-message_input');
     let chat = document.querySelector('.main-conversationPanel-chat');
     let userPanel = document.querySelector('.main-userPanel');
     let logInBtn = document.querySelector('.popup-form__btn');
     let authWindow = document.querySelector('.overlay');
-    let avatarWindow = document.querySelector('.overlay-foto');
-    let avatarResultWindow = document.querySelector('.overlay-foto-result');
-    let avatarResultWindowIcon = document.querySelector('#avatar-icon');
     let logInName = document.querySelector('#name').value;
     let logInNick = document.querySelector('#nick').value;
-    let newAvatar = document.querySelector('#myfile');
-    let avaSaveButton = document.querySelector('#ava-save');
-    let encodeImageFileAsURL;
 
     function logIn() {
         clientData = {
@@ -26,34 +20,26 @@ function initApp() {
             avatar: '',
             action: 'logInAct'
         };
+
         socket.send(JSON.stringify(clientData));
-        console.log('отправили на сервер залогиненного клиента', clientData);
+
         return clientData;
-
-
     }
 
     logInBtn.addEventListener('click', e => {
-        console.log('Нажата');
         logInName = document.querySelector('#name').value;
         logInNick = document.querySelector('#nick').value;
         if (logInName != '' && logInNick != '') {
             logIn();
-            // console.log(clientData);
-            // sendMessage();
             authWindow.style.display = 'none';
+        } else {
+            alert('Все поля должны быть заполнены!');
         }
     });
 
-
     function addMessage(message) {
-        console.log('Сообщение1:', message);
         message = JSON.parse(message);
-        console.log('Сообщение2:', message);
-
-
         if (Array.isArray(message)) {
-            let obj;
             message.forEach((obj) => {
                 createUserItem(obj);
             });
@@ -62,71 +48,42 @@ function initApp() {
         }
 
         function createUserItem(obj) {
-
             if (obj.action == 'messageAct') {
-                console.log('Сообщение:', obj);
-
                 let userItem = document.createElement('div');
                 let iconItem = document.createElement('div');
                 let nameItem = document.createElement('div');
                 let messageItem = document.createElement('div');
+
                 messageItem.className = 'message-item';
                 if (obj.name != clientData.name) {
                     userItem.style.textAlign = 'left';
                 }
                 messageItem.innerHTML = `${obj.message}  <i style="color: #aaaaaa">[${getTime()}]</i>`;
-
-
                 userItem.className = 'user-item-chat';
                 iconItem.className = 'icon-item-chat';
                 nameItem.className = 'name-item-chat';
-
-
                 nameItem.textContent = obj.name;
-
                 userItem.appendChild(iconItem);
                 userItem.appendChild(nameItem);
                 userItem.appendChild(messageItem);
-
                 chat.appendChild(userItem);
                 chat.scrollTop = chat.scrollHeight;
-
                 if (obj.avatar != '') {
                     iconItem.style.backgroundImage = `url(${obj.avatar})`;
-                    let allAvatars = document.querySelectorAll('.icon-item-chat');
-                    console.log('allAvatars:', allAvatars);
-
-                    // if (allAvatars) {
-                    //     for (let i = 0; i < allAvatars.length; i++) {
-                    //         if (obj.name == nameItem[i].textContent) {
-                    //             nameItem[i].previousElementSibling.style.backgroundImage = `url(${obj.avatar})`;
-                    //         }
-                    //     }
-                    // }
-
                 }
-
-
             }
 
             if (obj.action == 'logInAct') {
-                console.log('Юзер:', obj);
-                //clientData = obj;
-
                 let userItem = document.createElement('div');
                 let iconItem = document.createElement('div');
                 let nameItem = document.createElement('div');
 
-
                 userItem.className = 'user-item';
                 iconItem.className = 'icon-item';
                 nameItem.className = 'name-item';
-
                 nameItem.textContent = obj.name;
                 if (obj.avatar != '') {
                     iconItem.style.backgroundImage = `url(${obj.avatar})`;
-                    let allAvatars = document.querySelectorAll('.icon-item-chat');
-                    console.log('allAvatars:', allAvatars);
                 }
 
                 userItem.appendChild(iconItem);
@@ -134,11 +91,9 @@ function initApp() {
                 userPanel.appendChild(userItem);
 
                 let currentItemName = document.querySelector('#name-item-cur');
-                let currentAva = document.querySelector('#currentAva');
                 let currentAvaName = document.querySelector('#currentAvaName');
+
                 if (obj.name == currentItemName.textContent) {
-                    //currentItemIcon.style.backgroundImage = `url(${obj.avatar})`;
-                    //currentAva.style.backgroundImage = `url(${obj.avatar})`;
                     currentAvaName.textContent = obj.name;
                 }
             }
@@ -146,6 +101,7 @@ function initApp() {
             if (obj.action == 'currentUserAct') {
                 clientData = obj;
                 currName = obj.name;
+
                 let currentUserPlace = document.querySelector('.currentUserPlace');
                 let currentItemName = document.querySelector('#name-item-cur');
                 let currentItemIcon = document.querySelector('#icon-item-cur');
@@ -154,15 +110,10 @@ function initApp() {
                 currentUserPlace.style.display = 'block';
                 if (obj.avatar != '') {
                     currentItemIcon.style.backgroundImage = `url(${obj.avatar})`;
-                    let allAvatars = document.querySelectorAll('.icon-item-chat');
-                    console.log('allAvatars:', allAvatars);
                 }
-
-
             }
 
             if (obj.action == 'avatarAct') {
-                let currentUserPlace = document.querySelector('.currentUserPlace');
                 let currentItemName = document.querySelector('#name-item-cur');
                 let itemName = document.querySelectorAll('.name-item');
                 let currentItemIcon = document.querySelector('#icon-item-cur');
@@ -170,7 +121,6 @@ function initApp() {
                 for (let i = 0; i < itemName.length; i++) {
                     if (itemName[i].textContent == obj.name) {
                         itemName[i].previousElementSibling.style.backgroundImage = `url(${obj.avatar})`;
-                        // break;
                     }
                 }
                 let currentAva = document.querySelector('#currentAva');
@@ -179,14 +129,11 @@ function initApp() {
                     currentItemIcon.style.backgroundImage = `url(${obj.avatar})`;
                     currentAva.style.backgroundImage = `url(${obj.avatar})`;
                     currentAvaName.textContent = obj.name;
-
                 }
-
 
                 if (obj.avatar != '') {
                     let allAvatars = document.querySelectorAll('.icon-item-chat');
                     let nameItem = document.querySelectorAll('.name-item-chat');
-                    console.log('allAvatars:', allAvatars);
 
                     if (allAvatars) {
                         for (let i = 0; i < allAvatars.length; i++) {
@@ -195,40 +142,28 @@ function initApp() {
                             }
                         }
                     }
-
                 }
-
-
             }
 
             if (obj.amount) {
-                console.log('Кол-во онлайн:', obj);
-
                 let amount = document.querySelector('.amount');
                 amount.textContent = `ВСЕ ПОЛЬЗОВАТЕЛИ [${obj.amount}]:`;
             }
 
             if (obj.action == 'delAct') {
-                console.log('Ушел оффлайн:', obj);
-
                 let userItem = document.querySelectorAll('.user-item');
-                let nameItem = document.querySelectorAll('.name-item');
                 let userPanel = document.querySelector('.main-userPanel');
-
 
                 for (let i = 0; i < userPanel.children.length; i++) {
                     if (userItem[i].textContent == obj.name) {
                         userPanel.removeChild(userItem[i]);
                     }
                 }
-
             }
         }
     }
 
-
     socket.onmessage = function (e) {
-        console.log("Получили с сервера", e.data);
         addMessage(e.data);
     };
 
@@ -236,28 +171,15 @@ function initApp() {
         if (sendMessageInput.value != '') {
             clientData.message = sendMessageInput.value;
             clientData.action = 'messageAct';
-
-
             socket.send(JSON.stringify(clientData));
-            console.log(JSON.stringify(clientData));
             sendMessageInput.value = '';
         } else {
             alert('Отправлять пустое сообщение плохая идея :)');
         }
-
-
-
     }
 
     sendMessageButton.addEventListener('mousedown', sendMessage);
     sendMessageInput.addEventListener('change', sendMessage);
-
-
-
-
-    socket.onopen = function () {
-        //alert("Соединение установлено.");
-    };
 
     socket.onclose = function (event) {
         if (event.wasClean) {
@@ -268,14 +190,9 @@ function initApp() {
         alert('Код: ' + event.code + ' причина: ' + event.reason);
     };
 
-    // socket.onmessage = function (event) {
-    //     alert("Получены данные " + event.data);
-    // };
-
     socket.onerror = function (error) {
         alert("Ошибка " + error.message);
     };
-
 }
 
 encodeImageFileAsURL = function (element) {
@@ -283,14 +200,10 @@ encodeImageFileAsURL = function (element) {
     let avatarResultWindow = document.querySelector('.overlay-foto-result');
     let avatarResultWindowIcon = document.querySelector('#avatar-icon');
     let avaSaveButton = document.querySelector('#ava-save');
-    console.log("1", element.files[0]);
-    console.log("2", element.files);
-    var file = element.files[0];
-    var reader = new FileReader();
+    let file = element.files[0];
+    let reader = new FileReader();
+
     reader.onloadend = function () {
-        console.log('RESULT', reader.result);
-
-
         avatarResultWindowIcon.style.backgroundImage = `url(${reader.result})`;
         avatarWindow.style.display = 'none';
         avatarResultWindow.style.display = 'block';
@@ -301,20 +214,13 @@ encodeImageFileAsURL = function (element) {
                 avatar: reader.result,
                 action: 'avatarAct'
             };
+
             socket.send(JSON.stringify(clientData));
-            console.log('отправили на сервер аватарку для', clientData.name, reader.result);
             avatarResultWindow.style.display = 'none';
-        })
+        });
     };
     reader.readAsDataURL(file);
-
-
-
-
-}
-
-
-
+};
 
 function getTime() {
     let date = new Date(),
@@ -324,9 +230,5 @@ function getTime() {
 
     return currentTime;
 }
-
-
-
-
 
 window.onload = initApp;
--- js/server.js
+++ js/server.js
@@ -1,15 +1,9 @@
 const WebSocket = require('ws');
-
 const wss = new WebSocket.Server({
     port: 8080
 });
-
-let client;
-let message;
 let usersOnline = [];
 let usersOffline = [];
-let clients = [];
-let messages = [];
 let amountOnline = {
     amount: 0
 };
@@ -31,12 +25,9 @@ function createNewUser(data) {
                     }
                     if (user.action == 'currentUserAct') {
                         data.avatar = user.avatar;
-
-                        //user.action = 'logInAct';
                     }
                     if (user.avatar != data.avatar && data.avatar != '') {
                         user.avatar = data.avatar;
-                        //user.action = 'logInAct';
                     } else {
                         data.avatar = user.avatar;
                     }
@@ -96,186 +87,62 @@ function createNewUser(data) {
 }
 
 wss.on('connection', function connection(ws) {
-
-
     let user;
+
     ws.on('message', function incoming(data) {
-        // data = JSON.parse(data);
-        // console.log(data);
-        // console.log(data.name);
-        // console.log(data.nick);
-        // console.log(data.message);
-        // console.log(data.avatar);
-        // console.log(data.action);
-
-        // if (users == '') {
-        //     user = {
-        //         name: data.name || '',
-        //         nick: data.nick || '',
-        //         message: data.message || '',
-        //         avatar: data.avatar || '',
-        //         action: data.action || '',
-        //         status: 'online'
-        //     };
-        //     console.log("ПЕРВЫЙ");
-        // } else if (user.name != data.name) {
-        //     user = {
-        //         name: data.name || '',
-        //         nick: data.nick || '',
-        //         message: data.message || '',
-        //         avatar: data.avatar || '',
-        //         action: data.action || '',
-        //         status: 'online'
-        //     };
-        //     console.log("НОВЫЙ");
-        // } else {
-        //     console.log("СТАРЫЙ");
-        // }
         user = createNewUser(data);
-
-
-
-        // if (user.action == 'logInAct') {
-
-        //     users.push(user);
-        //     // clients.push(client);
-        //clients[user.name] = ws;
-        //     messages[user.name] = user.message;
-        //console.log("ТЕКУЩИЙ КЛИЕНТ:", clients[user.name], "\n");
-        // }
-
-        // if (user.action == 'messageAct') {
-        //     messages.push(user.message);
-        // }
-
-        console.log("Все юзеры:", usersOffline, "\n");
-        //console.log("Все клиенты:", clients, "\n");
-        //console.log("Все сообщения:", messages, "\n");
-
-
-
         wss.clients.forEach(function each(client) {
             if (client.readyState === WebSocket.OPEN) {
-
-
                 if (user.action == 'logInAct') {
                     amountOnline.amount = usersOnline.length;
                     let usersParsed = JSON.stringify(amountOnline);
-                    client.send(usersParsed);
 
+                    client.send(usersParsed);
                     if (usersOnline != '') {
                         if (client == ws) {
                             user.action = 'currentUserAct';
                             let usersParsed = JSON.stringify(user);
+
                             client.send(usersParsed);
                             user.action = 'logInAct';
-
                             usersParsed = JSON.stringify(usersOnline);
                             client.send(usersParsed);
                         } else {
                             let usersParsed = JSON.stringify(user);
+
                             client.send(usersParsed);
                         }
                     } else {
                         let usersParsed = JSON.stringify(user);
+
                         client.send(usersParsed);
                     }
                 }
 
                 if (user.action == 'messageAct') {
-
-                    //user.action = 'currentAct';
-                    console.log("currentName:", user.name, "\n");
-                    console.log("currentAct:", user.action, "\n");
                     let usersParsed = JSON.stringify(user);
-                    client.send(usersParsed);
 
+                    client.send(usersParsed);
                 }
 
                 if (user.action == 'avatarAct') {
-
                     let usersParsed = JSON.stringify(user);
+
                     client.send(usersParsed);
-
-
                 }
-
-
-                // usersOnline.forEach((item)=>{
-                //     if (user.name==item.name) {
-                //         let usersParsed = JSON.stringify(user);
-                //         client.send(usersParsed);
-                //     }
-                // })
-
-
-
-                // console.log('======');
-                // console.log("Все юзеры:", users);
-                // console.log('======');
-
-                // console.log('======');
-                // console.log("Все сообщения:", messages);
-                // console.log('======');
-
-                // if (user.action == 'logInAct' && user.status == 'online') {
-                //     let usersParsed = JSON.stringify(users);
-                //     client.send(usersParsed);
-
-                //     // user = JSON.stringify(user);
-                //     // client.send(user);
-                //     // console.log('logInAct', user);
-
-                // } else {
-                //     // user = JSON.stringify(user);
-                //     // //client.send(`${user.name} (${user.nick}): ${user.message}, ${user.avatar}, ${user.action}`);
-                //     // client.send(user);
-                //     // console.log('elseAct', user);
-                // }
-
-
-
             }
         });
     });
 
     ws.on('close', function close(e) {
-
-        //console.log('Сеанс закрыт', ws);
-        //console.log("Все юзеры:", users, "\n");
-        console.log("Все сообщения:", messages, "\n");
-        //console.log("Все клиенты ДО:", clients, "\n");
-        //user = JSON.parse(user);
-
-        // for (let userItem of users) {
-        //     if (user.name == userItem.name)
-        //         userItem.status = 'offline';
-        // }
-
-        //console.log("Все юзеры ОФФ:", users, "\n");
-        //console.log("Один юзер:", user, "\n");
-        //console.log("ТЕКУЩИЙ СТУТУС:", users[user.status], "\n");
-        //console.log("ТЕКУЩИЙ КЛИЕНТ:", clients[user.name], "\n");
-        //delete clients[user.name];
-        //delete usersOnline[user.name];
-        //console.log("Все клиенты ПОСЛЕ удаления:", clients, "\n");
-        console.log("Один юзер:", user, "\n");
-
-
         wss.clients.forEach(function each(client) {
-
             user.action = 'delAct';
             let usersParsed = JSON.stringify(user);
-            client.send(usersParsed);
 
+            client.send(usersParsed);
             usersOnline.forEach((item, i) => {
                 if (item.name == user.name) {
-                    console.log("i:", i, "\n");
-                    console.log("юзеры ДО:", usersOnline, "\n");
-                    let deleted = usersOnline.splice(i, 1);
-                    console.log("юзеры ПОСЛЕ:", usersOnline, "\n");
-                    console.log("УДАЛИЛИ:", deleted, "\n");
-                    //break;
+                    usersOnline.splice(i, 1);
                 }
             });
 
@@ -284,36 +151,22 @@ wss.on('connection', function connection(ws) {
                     item.action = user.action;
                 }
             });
+
             amountOnline.amount = usersOnline.length;
             usersParsed = JSON.stringify(amountOnline);
             client.send(usersParsed);
         });
-        //console.log("wss.clients:", wss.clients, "\n");
 
         if (wss.clients.size == 0) {
             if (user) {
                 user.action = 'delAct';
-
-                let deleted = usersOnline.splice(0, 1);
-
-                console.log("юзеры ПОСЛЕ:", usersOnline, "\n");
-                console.log("УДАЛИЛИ:", deleted, "\n");
-
+                usersOnline.splice(0, 1);
                 usersOffline.forEach((item, i) => {
                     if (item.name == user.name) {
                         item.action = user.action;
                     }
                 });
             }
-
-
         }
-
-        console.log("Кто онлайн:", usersOnline, "\n");
-        console.log("Полный список юзеров:", usersOffline, "\n");
-
     });
-
-    console.log("---------------------------------", "\n");
-    //console.log('Коннекшн', ws);
 });
