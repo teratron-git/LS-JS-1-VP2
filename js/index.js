8d538185dc8d80f677b808946214bfd50270fbd8
Tereshkov Sergey[L] <rat87@mail.ru>
2020-03-12 03:31:41 +0300

    all functions are ready, exept avatar

--- js/index.js
+++ js/index.js
@@ -7,11 +7,10 @@ function initApp() {
     let authWindow = document.querySelector('.overlay');
     let socket = new WebSocket('ws://localhost:8080');
     let clientData;
+    let logInName = document.querySelector('#name').value;
+    let logInNick = document.querySelector('#nick').value;
 
     function logIn() {
-        let logInName = document.querySelector('#name').value;
-        let logInNick = document.querySelector('#nick').value;
-
         clientData = {
             name: logInName,
             nick: logInNick,
@@ -22,16 +21,22 @@ function initApp() {
         socket.send(JSON.stringify(clientData));
         console.log('отправили на сервер залогиненного клиента', clientData);
         return clientData;
+
+
     }
 
     logInBtn.addEventListener('click', e => {
         console.log('Нажата');
-        logIn();
-        // console.log(clientData);
-        // sendMessage();
-        authWindow.style.display = 'none';
-
+        logInName = document.querySelector('#name').value;
+        logInNick = document.querySelector('#nick').value;
+        if (logInName != '' && logInNick != '') {
+            logIn();
+            // console.log(clientData);
+            // sendMessage();
+            authWindow.style.display = 'none';
+        }
     });
+
 
     function addMessage(message) {
         console.log('Сообщение1:', message);
@@ -41,9 +46,9 @@ function initApp() {
 
         if (Array.isArray(message)) {
             let obj;
-            message.forEach((obj)=> {
+            message.forEach((obj) => {
                 createUserItem(obj);
-            })
+            });
         } else {
             createUserItem(message);
         }
@@ -51,38 +56,83 @@ function initApp() {
         function createUserItem(obj) {
             if (obj.action == 'messageAct') {
                 console.log('Сообщение:', obj);
-    
-    
-                let messageItem = document.createElement('div');
-                messageItem.className = 'message-item';
-                messageItem.textContent = `${obj.name} (${obj.nick}): ${obj.message}, ${obj.avatar}, ${obj.action}`;
-    
-                chat.appendChild(messageItem);
-                chat.scrollTop = chat.scrollHeight;
-            }
-    
-            if (obj.action == 'logInAct') {
-                console.log('Юзер:', obj);
-                clientData = obj;
-    
+
                 let userItem = document.createElement('div');
                 let iconItem = document.createElement('div');
                 let nameItem = document.createElement('div');
-    
-    
+                let messageItem = document.createElement('div');
+                messageItem.className = 'message-item';
+                if (obj.name != clientData.name) {
+                    userItem.style.textAlign = 'left';
+                }
+                messageItem.innerHTML = `${obj.message}  <i style="color: #aaaaaa">[${getTime()}]</i>`;
+
+
+                userItem.className = 'user-item-chat';
+                iconItem.className = 'icon-item-chat';
+                nameItem.className = 'name-item-chat';
+
+                nameItem.textContent = obj.name;
+
+                userItem.appendChild(iconItem);
+                userItem.appendChild(nameItem);
+                userItem.appendChild(messageItem);
+
+                chat.appendChild(userItem);
+                chat.scrollTop = chat.scrollHeight;
+            }
+
+            if (obj.action == 'logInAct') {
+                console.log('Юзер:', obj);
+                //clientData = obj;
+
+                let userItem = document.createElement('div');
+                let iconItem = document.createElement('div');
+                let nameItem = document.createElement('div');
+
+
                 userItem.className = 'user-item';
                 iconItem.className = 'icon-item';
                 nameItem.className = 'name-item';
-    
+
                 nameItem.textContent = obj.name;
-    
+
                 userItem.appendChild(iconItem);
                 userItem.appendChild(nameItem);
                 userPanel.appendChild(userItem);
             }
 
+            if (obj.action == 'currentUserAct') {
+                clientData = obj;
+                let currentUserPlace = document.querySelector('.currentUserPlace');
+                let currentItem = document.querySelector('#name-item-cur');
+
+                currentItem.textContent = obj.name;
+                currentUserPlace.style.display = 'block';
+
+            }
+
+            if (obj.amount) {
+                console.log('Кол-во онлайн:', obj);
+
+                let amount = document.querySelector('.amount');
+                amount.textContent = `ВСЕ ПОЛЬЗОВАТЕЛИ [${obj.amount}]:`;
+            }
+
             if (obj.action == 'delAct') {
-                // Удаление надо реализовать
+                console.log('Ушел оффлайн:', obj);
+
+                let userItem = document.querySelectorAll('.user-item');
+                let nameItem = document.querySelectorAll('.name-item');
+                let userPanel = document.querySelector('.main-userPanel');
+
+
+                for (let i = 0; i < userPanel.children.length; i++) {
+                    if (userItem[i].textContent == obj.name) {
+                        userPanel.removeChild(userItem[i]);
+                    }
+                }
+
             }
         }
     }
@@ -94,19 +144,23 @@ function initApp() {
     };
 
     function sendMessage() {
+        if (sendMessageInput.value != '') {
+            clientData.message = sendMessageInput.value;
+            clientData.action = 'messageAct';
 
-        clientData.message = sendMessageInput.value;
-        clientData.action = 'messageAct';
 
+            socket.send(JSON.stringify(clientData));
+            console.log(JSON.stringify(clientData));
+            sendMessageInput.value = '';
+        } else {
+            alert('Отправлять пустое сообщение плохая идея :)');
+        }
 
-        socket.send(JSON.stringify(clientData));
-        console.log(JSON.stringify(clientData));
-        sendMessageInput.value = '';
 
 
     }
 
-    sendMessageButton.addEventListener('click', sendMessage);
+    sendMessageButton.addEventListener('mousedown', sendMessage);
     sendMessageInput.addEventListener('change', sendMessage);
 
 
@@ -137,5 +191,13 @@ function initApp() {
 
 }
 
+function getTime() {
+    let date = new Date(),
+        hours = (date.getHours() < 10) ? `0${date.getHours()}` : date.getHours(),
+        minutes = (date.getMinutes() < 10) ? `0${date.getMinutes()}` : date.getMinutes(),
+        currentTime = `${hours}:${minutes}`;
+
+    return currentTime;
+}
 
 window.onload = initApp;
--- js/server.js
+++ js/server.js
@@ -10,12 +10,15 @@ let usersOnline = [];
 let usersOffline = [];
 let clients = [];
 let messages = [];
+let amountOnline = {
+    amount: 0
+};
 
 function createNewUser(data) {
     data = JSON.parse(data);
     let user;
 
-    if (data.action == 'logInAct' || data.action == 'delAct') {
+    if (data.action == 'logInAct' || data.action == 'delAct' || data.action == 'currentUserAct' || data.action == 'messageAct') {
         let isOldUser = false;
 
         if (usersOffline != '') {
@@ -25,6 +28,9 @@ function createNewUser(data) {
                     if (user.action == 'delAct') {
                         user.action = 'logInAct';
                         usersOnline.push(user);
+                    }
+                    if (user.action == 'currentUserAct') {
+                        user.action = 'logInAct';
                     }
                     isOldUser = true;
                     break;
@@ -59,7 +65,17 @@ function createNewUser(data) {
             usersOffline.push(user);
         }
     }
-    
+
+    if (data.action == 'messageAct') {
+        user = {
+            name: data.name || '',
+            nick: data.nick || '',
+            message: data.message || '',
+            avatar: data.avatar || '',
+            action: data.action || ''
+        };
+    }
+
     return user;
 }
 
@@ -124,19 +140,42 @@ wss.on('connection', function connection(ws) {
 
         wss.clients.forEach(function each(client) {
             if (client.readyState === WebSocket.OPEN) {
-                
-                if (usersOnline != '') {
-                    if (client == ws) {
-                        let usersParsed = JSON.stringify(usersOnline);
-                        client.send(usersParsed);
+
+
+                if (user.action == 'logInAct') {
+                    amountOnline.amount = usersOnline.length;
+                    let usersParsed = JSON.stringify(amountOnline);
+                    client.send(usersParsed);
+
+                    if (usersOnline != '') {
+                        if (client == ws) {
+                            user.action = 'currentUserAct';
+                            let usersParsed = JSON.stringify(user);
+                            client.send(usersParsed);
+                            user.action = 'logInAct';
+
+                            usersParsed = JSON.stringify(usersOnline);
+                            client.send(usersParsed);
+                        } else {
+                            let usersParsed = JSON.stringify(user);
+                            client.send(usersParsed);
+                        }
                     } else {
                         let usersParsed = JSON.stringify(user);
                         client.send(usersParsed);
                     }
-                } else {
+                }
+
+                if (user.action == 'messageAct') {
+
+                    //user.action = 'currentAct';
+                    console.log("currentName:", user.name, "\n");
+                    console.log("currentAct:", user.action, "\n");
                     let usersParsed = JSON.stringify(user);
                     client.send(usersParsed);
+
                 }
+
 
                 // usersOnline.forEach((item)=>{
                 //     if (user.name==item.name) {
@@ -146,7 +185,7 @@ wss.on('connection', function connection(ws) {
                 // })
 
 
-                
+
                 // console.log('======');
                 // console.log("Все юзеры:", users);
                 // console.log('======');
@@ -209,21 +248,24 @@ wss.on('connection', function connection(ws) {
                 if (item.name == user.name) {
                     console.log("i:", i, "\n");
                     console.log("юзеры ДО:", usersOnline, "\n");
-                    let deleted = usersOnline.splice(i,1);
+                    let deleted = usersOnline.splice(i, 1);
                     console.log("юзеры ПОСЛЕ:", usersOnline, "\n");
                     console.log("УДАЛИЛИ:", deleted, "\n");
                     //break;
                 }
-            })
+            });
+            amountOnline.amount = usersOnline.length;
+            usersParsed = JSON.stringify(amountOnline);
+            client.send(usersParsed);
         });
-        console.log("wss.clients:", wss.clients, "\n");
+        //console.log("wss.clients:", wss.clients, "\n");
 
-        if(wss.clients.size == 0) {
+        if (wss.clients.size == 0) {
             user.action = 'delAct';
 
-                    let deleted = usersOnline.splice(0,1);
-                    console.log("юзеры ПОСЛЕ:", usersOnline, "\n");
-                    console.log("УДАЛИЛИ:", deleted, "\n");
+            let deleted = usersOnline.splice(0, 1);
+            console.log("юзеры ПОСЛЕ:", usersOnline, "\n");
+            console.log("УДАЛИЛИ:", deleted, "\n");
 
         }
 
